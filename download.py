15a02fde56542f6560c1106d65e56147742dd271
Alexandre CUER <alexandre.cuer@cerema.fr>
2022-07-13 19:19:59 +0200

    Delete download.py

--- download.py
+++
@@ -1,7 +0,0 @@
-from contextlib import closing
-from urllib.request import urlopen
-
-print("downloading emonpi backup")
-url = "http://dromotherm.ddns.net/backup/download?apikey=9664269b8d334ec24d4b686998a8ec86"
-with closing(urlopen(url)) as b:
-    open('backup.tar.gz', 'wb').write(b.content)
