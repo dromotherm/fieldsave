d242a0408fe7374055e417f3395a5cceb07409f7
Alexandre CUER <alexandre.cuer@cerema.fr>
2023-08-09 16:45:54 +0200

    Update start.py

--- start.py
+++ start.py
@@ -2,9 +2,8 @@ from contextlib import closing
 from urllib.request import urlopen
 import os
 APIKEY=os.environ.get("RWKEY")
-print(APIKEY)
 
 print("starting emonpi backup")
-url = "http://dromotherm.ddns.net/backup/start?apikey=APIKEY"
+url = f'http://dromotherm.ddns.net/backup/start?apikey={APIKEY}'
 with closing(urlopen(url)) :
     pass
