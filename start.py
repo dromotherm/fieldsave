dfaf5f889d0408f1d5a57afd3351fb9424e41802
Alexandre CUER <alexandre.cuer@cerema.fr>
2023-08-09 16:19:11 +0200

    Update start.py

--- start.py
+++ start.py
@@ -1,7 +1,12 @@
 from contextlib import closing
 from urllib.request import urlopen
+import argparse
+
+parser = argparse.ArgumentParser()
+parser.add_argument("--key", action="store")
+p_args = parser.parse_args()
 
 print("starting emonpi backup")
-url = "http://dromotherm.ddns.net/backup/start?apikey=9664269b8d334ec24d4b686998a8ec86"
+url = "http://dromotherm.ddns.net/backup/start?apikey=p_args.key"
 with closing(urlopen(url)) :
     pass
